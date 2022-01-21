af7e4c18a39dd7f82629f0388ded87ba78ed4bdb
Ansul Agrawal <ansulagrawal9@gmail.com>
2022-01-21 14:54:00 +0530

    Updated Icon

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,31 @@
 import React, { useEffect, useState } from 'react';
 import './Navbar.css';
+import NextflixLogo from '../images/netflix.png';
 
 function Navbar() {
-  const [show, handleShow] = useState(false);
+   const [show, handleShow] = useState(false);
 
-  useEffect(() => {
-    window.addEventListener("scroll", () => {
-      if (window.scrollY > 100) {
-        handleShow(true);
-      } else handleShow(false);
-    });
-    return () => {
-      window.removeEventListener("scroll");
-    }
-  }, [])
+   useEffect(() => {
+      window.addEventListener('scroll', () => {
+         if (window.scrollY > 100) {
+            handleShow(true);
+         } else handleShow(false);
+      });
+      return () => {
+         window.removeEventListener('scroll');
+      };
+   }, []);
 
-  return (
-    <div className={`nav ${show && "nav_black"}`}>
-      <img className="nav_logo" src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png" alt="Netflix logo" />
-      <img className="nav_avatar" src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png" alt="" />
-    </div>
-  )
+   return (
+      <div className={`nav ${show && 'nav_black'}`}>
+         <img className='nav_logo' src={NextflixLogo} alt='Netflix logo' />
+         <img
+            className='nav_avatar'
+            src='https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png'
+            alt=''
+         />
+      </div>
+   );
 }
 
-export default Navbar
+export default Navbar;
